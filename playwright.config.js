a922c2c62b3465eb1fc22d0c193764f078af4d3f
Sumalatha <sumateegala06@gmail.com>
2025-12-20 19:08:38 -0500

    Enabled only Chrome browser tests

--- playwright.config.js
+++ playwright.config.js
@@ -59,23 +59,23 @@ export default defineConfig({
       
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'],
-        // Use prepared auth state.
-        storageState: 'playwright/.auth/user.json',
-       },
-    dependencies: ['setup'],
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'],
+    //     // Use prepared auth state.
+    //     storageState: 'playwright/.auth/user.json',
+    //    },
+    // dependencies: ['setup'],
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'],
-        // Use prepared auth state.
-        storageState: 'playwright/.auth/user.json',
-       },
-      dependencies: ['setup'],
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'],
+    //     // Use prepared auth state.
+    //     storageState: 'playwright/.auth/user.json',
+    //    },
+    //   dependencies: ['setup'],
+    // },
 
     /* Test against mobile viewports. */
     // {
